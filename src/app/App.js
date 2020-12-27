49b76ec8d2f5cc3d0b5e37eae39930e95b8524f9
woodrejs <woodrejs@gmaail.com>
2020-12-27 11:51:07 +0100

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledApp = styled.main`
+  margin: 0 auto;
+  max-width: 920px;
+  width: 100%;
+  min-height: 100vh;
+`;
+
+const App = () => {
+  return <StyledApp></StyledApp>;
+};
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/App";
+import { ThemeProvider } from "styled-components";
+import "./index.css";
+
+const theme = {
+  colors: {
+    first: "#FFFFFF",
+    secound: "#2B2A32",
+    third: "#D587C4",
+    fourth: "#E5C24D",
+    fifth: "#492B1D",
+  },
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
